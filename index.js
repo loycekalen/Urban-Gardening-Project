50742393a791059c5eb7c33dae5a7afa15a1f9a1
loycekalen <166914558+loycekalen@users.noreply.github.com>
2024-05-07 00:30:38 +0300

    made the webpage mobile responsive

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 function placeOrder() {
-  alert("Thank you for placing your order! We'll be in touch!");
+  let firstName = prompt("What is your name?");
+  let email = prompt("What is your email address?");
+  let seeds = prompt("What type of seeds do you want?");
+
+  let heading = document.querySelector("footer");
+
+  let question = `${firstName} ${email} ${seeds}`;
+  if (question === `${question}`) {
+    heading.innerHTML = "Thank you for placing your order! We'll be in touch!";
+  }
 }
 
-let orderButton = document.querySelector("button");
+let orderButton = document.querySelector("#order-button");
 
 orderButton.addEventListener("click", placeOrder);
